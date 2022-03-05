61a65e84f63301c50cd328057980b9736ba7647b
cbr <2366642046@qq.com>
2022-03-05 11:23:56 +0800

    2022.3.5.2

--- 剑指Offer/src/replaceSpace_005/Solution.java
+++ 剑指Offer/src/replaceSpace_005/Solution.java
@@ -4,5 +4,20 @@ package replaceSpace_005;
  * @author ASUS
  * @ 2022/3/5- 11:02
  */
-public class Solution {
+class Solution {
+    public String replaceSpace(String s) {
+
+
+        if (s.length()==0) return null;
+        StringBuilder stringBuilder =new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' '){
+                stringBuilder.append(s.charAt(i));
+
+            }else stringBuilder.append("%20");
+        }
+        return stringBuilder.toString();
+
+
+    }
 }
